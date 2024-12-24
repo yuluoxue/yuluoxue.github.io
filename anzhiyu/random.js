a78b67acbc4c78102fd0ee23ab6d38cc44459726
yuluoxue <s3056335670@gmail.com>
2024-12-25 00:23:09 +0800

    Site updated: 2024-12-25 00:23:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/24/hello-world/"];function toRandomPost(){
+var posts=["2024/12/24/hello-world/","2024/12/25/s/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
